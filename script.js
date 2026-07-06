e7fa74d815fdcd71efc680bb54dbe9f03eae4944
greeshmamadhu25-lang <greeshmamadhu25@gmail.com>
2026-07-06 11:30:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,48 @@
-document.getElementById("enterBtn").addEventListener("click", function () {
+const message = `Hi Baby,
 
-    window.location.href = "letters.html";
+Before you begin this new chapter of your life...
 
-});
+I wanted to leave you with something.
+
+It's not that perfect okay.
+
+Just something made with love.
+
+Whenever you miss me...
+
+Whenever life feels overwhelming...
+
+Whenever you simply want to feel close to me...
+
+Come back here.
+
+A part of my heart will always be waiting for you.
+
+❤️`;
+
+let i = 0;
+
+function typeWriter() {
+
+    if (i < message.length) {
+
+        document.getElementById("typing").innerHTML +=
+            message.charAt(i) === "\n"
+                ? "<br>"
+                : message.charAt(i);
+
+        i++;
+
+        setTimeout(typeWriter,40);
+
+    }
+
+}
+
+typeWriter();
+
+document.getElementById("enterBtn").onclick = function(){
+
+    window.location.href="letters.html";
+
+};
